fa141186717f64d6d757e1a0cac26e16528e58e4
森部高昌 <takamasamoribe@gmail.com>
2026-05-03 20:25:10 +0900

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by 森部高昌 on 2026/05/03.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
